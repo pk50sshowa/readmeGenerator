b669fd8a2cb8ad32d8bd01826704b7970ade9daa
Paul Koehler <paul.koehler@gmail.com>
2023-04-17 11:24:33 -0700

    first commit, questions array created

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?',
+    'Please give a basic description of your project.',
+    'Please explain how to install this application.',
+    'Please explain application user information.',
+    'Please explain contribution guidelines.',
+    'Please give test instructions.'
+                ];
+
+console.log (questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
